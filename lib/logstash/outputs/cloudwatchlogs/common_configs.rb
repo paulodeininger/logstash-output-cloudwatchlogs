8ecd5d91657bfb3d999c8755399e76fe7cc53ca4
Paulo Eduardo Alves <paulo.alves@velocitycloud.com>
2018-03-14 20:50:23 -0300

    Revert "log_stream_name using variables"
    This reverts commit dadbc7577c0e69fbe693537525832f3d461101c6.

--- lib/logstash/outputs/cloudwatchlogs/common_configs.rb
+++
@@ -1,9 +0,0 @@
-require 'forwardable'
-
-module LogStash; module Outputs; class CloudWatchLogs
-  module CommonConfigs
-    def self.included(mod)
-      mod.config :log_stream_name, :validate => :string, :required => true
-    end
-  end
-end end end
